a04f01124541d93900dfc49f894bcd12098f9ef9
Veduanon <dimashquwert@gmail.com>
2022-08-05 00:35:55 +0600

    Ds

--- admin/js/index.js
+++ admin/js/index.js
@@ -9,9 +9,6 @@ const calculator = document.querySelector(".calculator")
 const loginInputs = document.getElementById("login-inputs")
 const userOrders = document.querySelector(".user_orders")
 
-function gener() {
-    showProducts();
-}
 
 function createAdmin(cancelAd) {
     let makeAdminForm = document.getElementById("make-admin-form")
@@ -40,34 +37,61 @@ function createAdmin(cancelAd) {
 //                                  START PERSONAL Cabinet
 function personalCabinet(user) {
     try {
-        if (user) {
-            status.innerHTML = `
-        <h3>Customer</h3>
-        `
-            db.collection('users').doc(user.uid).get().then((doc) => {
-                const html = `
-            <div> ${user.email} </div>
-            <p> ${doc.data().first_name}</p>
-            <p> ${doc.data().second_name}</p>
-            <input placeholder="Адрес" value='${doc.data().addres}'>
-            <input placeholder="Город" value='${doc.data().city}'>
-            <input placeholder="Номер телефона" value='${doc.data().phoneNumber}'>
-            <button id="change">Изменить</button>
-            `
-                statusEmail.innerHTML = html
-                change.addEventListener('click', (e) => {
-                    db.collection('users').doc(user.uid).update({
-                        addres: statusEmail.children[3].value,
-                        city: statusEmail.children[4].value,
-                        phoneNumber: statusEmail.children[5].value,
-                    }).then(() => {
-                        console.log("Изменения внесены")
+        db.collection('users').doc(user.uid).get().then(doc => {
+            if (doc.data().isAdmin === false) {
+                status.innerHTML = `
+                <h3>Покупатель</h3>
+                `
+                db.collection('users').doc(user.uid).get().then((doc) => {
+                    const html = `
+                <div> ${user.email} </div>
+                <p> ${doc.data().first_name}</p>
+                <p> ${doc.data().second_name}</p>
+                <input placeholder="Адрес" value='${doc.data().addres}'>
+                <input placeholder="Город" value='${doc.data().city}'>
+                <input placeholder="Номер телефона" value='${doc.data().phoneNumber}'>
+                <button id="change">Изменить</button>
+                <button onclick="signOut()">Выход</button>
+                `
+                    statusEmail.innerHTML = html
+                    change.addEventListener('click', (e) => {
+                        db.collection('users').doc(user.uid).update({
+                            addres: statusEmail.children[3].value,
+                            city: statusEmail.children[4].value,
+                            phoneNumber: statusEmail.children[5].value,
+                        }).then(() => {
+                            console.log("Изменения внесены")
+                        })
                     })
                 })
-            })
-        } else {
-            status.innerHTML = `<h3>Guest</h3>`
-        }
+            } else if (doc.data().isAdmin === true) {
+                status.innerHTML = `
+            <h3>Ваш статус: Менеджер</h3><a href='../admin/adminpanel.html'>Перейти</a></h3>
+            `
+                db.collection('users').doc(user.uid).get().then((doc) => {
+                    const html = `
+                <div> ${user.email} </div>
+                <p> ${doc.data().first_name}</p>
+                <p> ${doc.data().second_name}</p>
+                <input placeholder="Адрес" value='${doc.data().addres}'>
+                <input placeholder="Город" value='${doc.data().city}'>
+                <input placeholder="Номер телефона" value='${doc.data().phoneNumber}'>
+                <button id="change">Изменить</button>
+                <button onclick="signOut()">Выход</button>
+                `
+                    statusEmail.innerHTML = html
+                    change.addEventListener('click', (e) => {
+                        db.collection('users').doc(user.uid).update({
+                            addres: statusEmail.children[3].value,
+                            city: statusEmail.children[4].value,
+                            phoneNumber: statusEmail.children[5].value,
+                        }).then(() => {
+                            console.log("Изменения внесены")
+                        })
+                    })
+                })
+            }
+        })
     } catch (e) { }
 }
 //                                  END PERSONAL Cabinet
@@ -150,7 +174,7 @@ function addProduct() {
     db.collection("products").add({
         header: createProductForm.children[0].value,
         text: createProductForm.children[1].value,
-    }).then(function(docRef) {
+    }).then(function (docRef) {
         createProductForm.children[4].innerHTML = "Товар добавлен"
         file = createProductForm.children[2].files[0];
         storageRef = firebase.storage().ref(`${file.name}`);
@@ -161,37 +185,61 @@ function addProduct() {
                 }).catch(() => {
                     console.log("Изображение не добавлено")
                 })
-        })
+            })
         })
     }
     )
 }
 
-function showProducts() {
-    db.collection("products").get().then(snapshot => {
-        try {
-            let products = document.querySelector(".product-list")
-            snapshot.docs.forEach(function callback(doc, index) {
-                const html = `
-            <div style="border: 5px solid;" class="product${index}">
-            <p>${doc.data().header}</p>
-            <p>${doc.data().text}</p>
-            <img src="${doc.data().image}">
-            <button id="btn${index}">Удалить</button>
-            </div>
-            `
-
-            products.innerHTML += html
-                    document.getElementById("btn" + index).addEventListener('click', (e) => {
-                        console.log(doc.id)
-                        db.collection("products").doc(doc.id).delete().then(() => {
-                            console.log("Товар удален");
-                        })
+function showProducts(data) {
+    let products = document.querySelector(".product-list")
+    try {
+        let html = '';
+        data.forEach(function callback(doc, index) {
+            const list = `<div style="border: 5px solid;" class="product${index}">
+                <input value="${doc.data().header}">
+                <input value="${doc.data().text}">
+                <img style="width:100px; height:100px;" src="${doc.data().image}">
+                <input type="file">
+                <button id="btnd${index}">Удалить</button>
+                <button id="btnu${index}">Редактировать</button>
+                </div>`
+                html += list
+                firebase.auth().onAuthStateChanged((user) => {
+                    if (user) {
+                        document.querySelector(".product" + index).addEventListener('click', (e) => {
+                            if (e.target.id == "btnd" + index) {
+                                db.collection("products").doc(doc.id).delete().then(() => {
+                                    console.log("Товар удален")
+                                })
+                            }
+                            if (e.target.id == "btnu" + index) {
+                                file = document.querySelector(".product" + index).children[3].files[0];
+                                storageRef = firebase.storage().ref(`${file.name}`);
+                                storageRef.put(file).then(() => {
+                                    storageRef.getDownloadURL().then((url) => {
+                                        db.collection("products").doc(doc.id).update({
+                                            image: url
+                                        }).catch(() => {
+                                            console.log("Изображение не добавлено")
+                                        })
+                                    })
+                                })
+                            db.collection("products").doc(doc.id).update({
+                                header: document.querySelector(".product" + index).children[0].value,
+                                text: document.querySelector(".product" + index).children[1].value,
+                            }).then(() => {
+                                console.log("Товар обновлен")
+                            })
+                        }
                     })
+                }
             })
-        } catch (e) {
-            console.log(e)
-        }
-    })
+        })
+        products.innerHTML = html
+
+    } catch (e) {
+        console.log(e)
+    }
 }
 //                                  END ADD PRODUCTS
